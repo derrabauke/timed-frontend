13c781dc720261fa0b9fc5e307dfd7de37b73136
Falk <falk.neumann@adfinis.com>
2022-10-11 21:20:55 +0200

    refactor(sy-calendar): refactor to octane

--- tests/integration/components/sy-calendar/component-test.js
+++ tests/integration/components/sy-calendar/component-test.js
@@ -1,6 +1,5 @@
 import { fillIn, render } from "@ember/test-helpers";
 import { setupRenderingTest } from "ember-qunit";
-import wait from "ember-test-helpers/wait";
 import hbs from "htmlbars-inline-precompile";
 import moment from "moment";
 import { module, test } from "qunit";
@@ -12,37 +11,39 @@ module("Integration | Component | sy calendar", function (hooks) {
     assert.expect(1);
 
     this.set("center", moment({ y: 2017, m: 10, d: 7 }));
+    this.set("onCenterChangeHandler", (value) => {
+      this.center = value.moment;
+    });
 
     await render(hbs`
-      {{sy-calendar
-        center=center
-        onCenterChange=(action (mut center) value='moment')
-      }}
+      <SyCalendar
+        @center={{this.center}}
+        @onCenterChange={{this.onCenterChangeHandler}}
+      />
     `);
 
     await fillIn(".nav-select-year select", "2010");
 
-    return wait().then(() => {
-      assert.equal(this.center.year(), 2010);
-    });
+    assert.strictEqual(this.center.year(), 2010);
   });
 
   test("can select a month", async function (assert) {
     assert.expect(1);
 
     this.set("center", moment({ y: 2017, m: 10, d: 7 }));
+    this.set("onCenterChangeHandler", (value) => {
+      this.center = value.moment;
+    });
 
     await render(hbs`
-      {{sy-calendar
-        center=center
-        onCenterChange=(action (mut center) value='moment')
-      }}
+      <SyCalendar
+        @center={{this.center}}
+        @onCenterChange={{this.onCenterChangeHandler}}
+      />
     `);
 
     await fillIn(".nav-select-month select", "May");
 
-    return wait().then(() => {
-      assert.equal(this.center.month(), 4);
-    });
+    assert.strictEqual(this.center.month(), 4);
   });
 });
